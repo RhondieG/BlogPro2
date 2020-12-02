a71e529f1b3e7f9a74ab94c327b75226a7957a94
Rhonda Gilliard <amourg08@gmail.com>
2020-12-02 14:11:21 -0500

    adding parser

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/blog', {
